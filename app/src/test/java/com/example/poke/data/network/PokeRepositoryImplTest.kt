39df13851cbb467c26eebbcf87a4becfe1045a5f
andru196 <andru196@icloud.com>
2022-01-08 02:24:45 +0300

    how to search

--- app/src/test/java/com/example/poke/data/network/PokeRepositoryImplTest.kt
+++ app/src/test/java/com/example/poke/data/network/PokeRepositoryImplTest.kt
@@ -9,9 +9,24 @@ class PokeRepositoryImplTest : TestCase() {
     fun testGet1() {
         val rep = PokeRepositoryImpl(PokeApiClient())
         val l = rep.get1()
-
-
         Asserts.checkNull(3)
+    }
 
+    fun testGet3() {
+        val rep = PokeRepositoryImpl(PokeApiClient())
+        val l = rep.getAllPokemonsList()
+        Asserts.checkNull(3)
+    }
+
+
+    fun testSearch() {
+        val searchText = "ba"
+
+        val rep = PokeRepositoryImpl(PokeApiClient())
+        val l = rep.getAllPokemonsList()
+        val needs = l.filter { x -> x.name.contains(searchText) }
+            .map { x -> x.id }
+            .toIntArray()
+        val rez = rep.getPokemons(ids = needs)
     }
 }
